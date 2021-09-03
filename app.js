b8dd4e7775762c0864f01504ea8ccf746b522b92
Victor Campos <vccacampos@outlook.com>
2021-09-02 22:38:22 -0300

    About start, navbar in all pages ok

--- app.js
+++ app.js
@@ -3,7 +3,20 @@ const express = require('express');
 const app = express();
 
 
-app.get('/home', (request, response, next) => {
+app.get('/', (request, response, next) => {
     
     response.sendFile(`${__dirname}/views/home.html`);
   });
+  app.get('/works', (request, response, next) => {
+    
+    response.sendFile(`${__dirname}/views/works.html`);
+  });
+  app.get('/about', (request, response, next) => {
+    
+    response.sendFile(`${__dirname}/views/about.html`);
+  });
+  app.get('/photoGallery', (request, response, next) => {
+    
+    response.sendFile(`${__dirname}/views/photoGallery.html`);
+  });
+  app.listen(3000, () => console.log('My first app listening on port 3000! '));
